7b7512f639ca4f2726b0c069e4f9350cc25404cc
turanulus <turanulus@gmail.com>
2025-05-14 07:26:10 +0300

    deneme

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/demo")
+@RequestMapping("/demo")
+@RestController
 public class DemoController {
 
     @GetMapping
     public String demo() {
-        return "";
+        return "deneme";
     }
 }
